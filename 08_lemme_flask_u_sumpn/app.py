1decb14a7320ec2aad39a108836eeb1a287bb891
Terran <terran@localhost.localdomain>
2018-09-19 21:32:51 -0400

    Changes to be committed: 	modified:   app.py
    By the way this only runs on python 2.7, there is something wrong with
    my linux vm and I need sometime to deal with it.

--- 08_lemme_flask_u_sumpn/app.py
+++ 08_lemme_flask_u_sumpn/app.py
@@ -1,1 +1,22 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return """
+        <h1>Hello!<h1>
+        <a href='/next'>Please visit the next page</a> """
+
+@app.route('/next')
+def hasif():
+    return """<h1>Hello again!<h1>
+        <a href='/next2'>Please visit the last page</a>"""
+
+@app.route('/next2')
+def route3():
+    return """<h1>Hello once more!<h1>
+        <a href="www.youtube.com">Please don't click on this link</a>"""
+
+app.debug = True
+app.run()
 
