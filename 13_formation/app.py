818dec3e77fb7c0b4d0a10a72edf67aacec39c8a
tianrunliu <tliu3@stuy.edu>
2018-09-27 21:22:14 -0400

    Changes to be committed: 	new file:   app.py 	new file:   templates/index.html 	new file:   templates/response.html

---
+++ 13_formation/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def init():
+    return render_template('index.html')
+
+@app.route('/output')
+def respond():
+    print (request)
+    print (request.headers)
+    print (request.method)
+    print (request.args)
+    print (request.form)
+    return render_template('response.html',response=request.args.get('item'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
