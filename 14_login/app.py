4b2fa09a8c3bc91272d8e07dd259b4694df305bd
tianrunliu <tliu3@stuy.edu>
2018-10-01 19:12:47 -0400

    It kinda works...

--- 14_login/app.py
+++ 14_login/app.py
@@ -13,20 +13,34 @@ def home():
 	print(request.args)
 	print("xxx DIAG:form xxx")
 	print(request.form)
-	if 'alan' in session:
-		return render_template('welcome.html',user = request.args['usr'])
+	print("xxx DIAG:session xxx")
+	print(session)
+	if "username" in session:
+		return render_template('welcome.html',user = session['username'])
 	else:
 		return render_template('home.html')
 
 @app.route('/login')
 def login():
 	if request.args['usr'] == 'Alan Smith' and request.args['pwd']=='password12345678':
-		session['usr'] = "alan"
+		print("xxx DIAG:args xxx")
+		print(request.args)
+		print("xxx DIAG:form xxx")
+		print(request.form)
+		session['username'] = "Alan Smith"
+		print("xxx DIAG:session xxx")
+		print(session)
 		return redirect(url_for('home'))
 	elif request.args.get['usr'] == 'Alan Smith' and request.form.get('pwd')!='password12345678':
 		return render_template('error.html',message='password wrong!')
 	else:
 		return render_template('error.html',message='username wrong!')
-	
 
-app.run(debug=True)
+@app.route('/logout')
+def logout():
+	session.pop('username',None)
+	return redirect(url_for('home'))
+
+	
+if __name__ == "__main__":
+	app.run(debug=True)
