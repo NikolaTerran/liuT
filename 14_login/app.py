38497841d3283d9ea5a9fff1c4ed9fd2db0be0e1
Terran Liu <38232187+TianrunLiu@users.noreply.github.com>
2018-10-01 22:39:56 -0400

    Update app.py

--- 14_login/app.py
+++ 14_login/app.py
@@ -1,3 +1,8 @@
+#Claire Liu and Tianrun Liu - Liu Squared
+#SoftDev1 pd6
+#K14 -- Do I know You?
+#2018-10-02
+
 from flask import Flask, render_template, request, session, redirect, url_for
 import os
 
@@ -16,13 +21,17 @@ def home():
 	print(request.form)
 	print("xxx DIAG:session xxx")
 	print(session)
+	#checks if there is a session
 	if "username" in session:
+                #if there is then just show the welcome screen
 		return render_template('welcome.html',user = session['username'])
 	else:
+                #if not just ask for info
 		return render_template('home.html')
 
 @app.route('/login')
 def login():
+        #Login: Alan Smith PW: password12345678, checks if it is correct
 	if request.args['usr'] == 'Alan Smith' and request.args['pwd']=='password12345678':
 		print("xxx DIAG:args xxx")
 		print(request.args)
@@ -32,6 +41,7 @@ def login():
 		print("xxx DIAG:session xxx")
 		print(session)
 		return redirect(url_for('home'))
+	# if either is wrong then it returns an error message
 	elif request.args['usr'] == 'Alan Smith' and request.args['pwd']!='password12345678':
 		return render_template('error.html',message='password wrong!')
 	else:
@@ -39,6 +49,7 @@ def login():
 
 @app.route('/logout')
 def logout():
+        #removes current session
 	session.pop('username',"Alan Smith")
 	print("xxx DIAG:session xxx")
 	print(session)
