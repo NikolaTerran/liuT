32f08633cdba1b6926a704a41e8177bc700bca9b
Terran Liu <38232187+TianrunLiu@users.noreply.github.com>
2018-10-09 11:50:56 -0400

    Create print_list.c

---
+++ stuff/hw/print_list.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//struct node { int i; struct node *next;};
+
+void print_list(struct node *list){
+	struct node count = list.next;	
+	do{	
+		print("%d\n",list.i);
+		count = list.next 
+	}while(count.next);
