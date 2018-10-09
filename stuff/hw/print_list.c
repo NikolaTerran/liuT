9f24000416a82648a936acaf549002a749df7f67
Terran Liu <38232187+TianrunLiu@users.noreply.github.com>
2018-10-09 11:52:58 -0400

    Update print_list.c

--- stuff/hw/print_list.c
+++ stuff/hw/print_list.c
@@ -3,8 +3,8 @@
 //struct node { int i; struct node *next;};
 
 void print_list(struct node *list){
-	struct node count = list.next;	
+	struct node *count = list->next;	
 	do{	
-		print("%d\n",list.i);
-		count = list.next 
+		print("%d\n",list->i);
+		count = list->next 
 	}while(count.next);
