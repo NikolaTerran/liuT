901b8f5a26a81b209003cd6d9593cf73a157e3ab
tianrunliu <tliu3@stuy.edu>
2018-11-04 23:39:22 -0500

    uh, I have to admit I'm really bad at this.

--- stuff/hw/insert_front.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "linkedlist.h"
-struct node * insert_front(struct node *list, int i){
-	struct node new_node = {i;list};
-	return new_node;
-}
--- stuff/hw/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "linkedlist.h"
-
-int main(){
-	struct node head = {10, 0};
-	print_list(&head);
-	return 0;
-}
