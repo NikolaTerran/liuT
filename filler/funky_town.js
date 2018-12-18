91ef9998cecb0854887474e42186cb6a9149b653
Nikola Terran <nikolaterranthe1@gmail.com>
2018-12-18 12:35:37 -0500

    s

---
+++ filler/funky_town.js
@@ -0,0 +1,23 @@
+var fact = function(x){
+    if(x < 2){
+        return 1;
+        }
+    else{
+        return x * fact(x - 1);
+        }
+}
+var fibonacci = function(x){
+	int a = 1;
+	int b = 0;
+	int c = 0;
+	if(x=a){
+		return 1;
+	}
+	else if(x=b){
+		return b;
+	}
+	else {
+	
+	}
+}	
+//fact();
