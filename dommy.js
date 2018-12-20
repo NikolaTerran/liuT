302d5126449c4c7f8f9b9a535c1bc4892fbbd0bb
Nikola Terran <nikolaterranthe1@gmail.com>
2018-12-20 12:33:18 -0500

    s

---
+++ dommy.js
@@ -0,0 +1,34 @@
+var add_item = document.getElementById("b");
+add_item.addEventListener('click',function(){
+	add_item = document.createElement("BUTTON");
+});
+
+
+var e_call = document.getElementById("e");
+         	var d_call = document.getElementById("d");
+			d_call.addEventListener('click',
+			function(){
+			randomStudent;
+			e_call.innerHTML = randomStudent();
+			});
+			
+			var a_call = document.getElementById("a");
+			a_call.addEventListener('click',function(){console.log(fact(document.getElementById("a_t").value));
+			e_call.innerHTML = fact(document.getElementById("a_t").value);}
+			);
+			
+			var b_call = document.getElementById("b");
+			b_call.addEventListener('click',function(){console.log(fibonacci(document.getElementById("b_t").value));
+			e_call.innerHTML = fibonacci(document.getElementById("b_t").value);});
+			
+			var c_call = document.getElementById("c");
+			c_call.addEventListener('click',function(){
+			console.log(gcd(document.getElementById("c_t1").value,
+			document.getElementById("c_t2").value));
+			e_call.innerHTML = 				
+			gcd(document.getElementById("c_t1").value,
+			document.getElementById("c_t2").value);});
+	
+		var button = document.getElementsByTagName("BUTTON");
+         a_call.addEventListener('mouseover',
+         function(e){console.log(e);});
